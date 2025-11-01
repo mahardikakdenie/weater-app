04310ec12baa0af87c0f042bff2b4e4216fbca46
mahardikakdenie <kucingemas744@gmail.com>
2025-11-01 15:51:17 +0700

    chore:setup chore repository fetch API

---
+++ lib/repository/weather_repository.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/foundation.dart'; // untuk debugPrint
+import 'package:weather_app/model/weather_model.dart';
+import 'package:weather_app/utils/dio_weather_client.dart';
+
+class WeatherRepository {
+  static final DioWeatherClient _client = DioWeatherClient();
+
+  static Future<WeatherResponse> getCurrentWeather({
+    required String lat,
+    required String lon,
+    required String apiKey,
+  }) async {
+    try {
+      debugPrint("Fetching weather data...");
+      final response = await _client.get(
+        'data/2.5/weather',
+        queryParameters: {
+          'lat': lat,
+          'lon': lon,
+          'appid': apiKey,
+          'units': 'metric',
+        },
+      );
+
+      // Pastikan response.data adalah Map<String, dynamic>
+      final weatherResponse = WeatherResponse.fromJson(response.data);
+      debugPrint("Parsed weather: $weatherResponse");
+      return weatherResponse;
+    } catch (e) {
+      debugPrint("Error in WeatherRepository: $e");
+      rethrow;
+    }
+  }
+}
