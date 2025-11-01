c3f2ff98035798496947fc9b1853153d0891bb49
mahardikakdenie <kucingemas744@gmail.com>
2025-11-01 20:00:08 +0700

    feat:add new repository for fetch forecast weather

--- lib/repository/weather_repository.dart
+++ lib/repository/weather_repository.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart'; // untuk debugPrint
+import 'package:weather_app/model/forecast_model.dart';
 import 'package:weather_app/model/weather_model.dart';
 import 'package:weather_app/utils/dio_weather_client.dart';
 
@@ -25,6 +26,32 @@ class WeatherRepository {
       // Pastikan response.data adalah Map<String, dynamic>
       final weatherResponse = WeatherResponse.fromJson(response.data);
       return weatherResponse;
+    } catch (e) {
+      debugPrint("Error in WeatherRepository: $e");
+      rethrow;
+    }
+  }
+
+  static Future<ForecastResponse> getForecastWeather({
+    required String lat,
+    required String lon,
+    required String apiKey,
+  }) async {
+    try {
+      debugPrint("Fetching weather data...");
+      final response = await _client.get(
+        '/data/2.5/forecast',
+        queryParameters: {
+          'lat': lat,
+          'lon': lon,
+          'appid': apiKey,
+          'units': 'metric',
+        },
+      );
+
+      // Pastikan response.data adalah Map<String, dynamic>
+      final foreCastResp = ForecastResponse.fromJson(response.data);
+      return foreCastResp;
     } catch (e) {
       debugPrint("Error in WeatherRepository: $e");
       rethrow;
