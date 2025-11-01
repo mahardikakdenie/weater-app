ca39d655f942416ecafd41ed6c863362968c2ee4
mahardikakdenie <kucingemas744@gmail.com>
2025-11-02 01:38:22 +0700

    chore:location repository for first time hit api for security, tidyup the code, and for developer experience when read this code

---
+++ lib/repository/location_repository.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/model/location_model.dart';
+import 'package:weather_app/utils/dio_weather_client.dart';
+
+class LocationRepository {
+  static final DioWeatherClient _client = DioWeatherClient();
+  static Future<List<LocationModel>> getLocation({
+    required String search,
+    required String limit,
+  }) async {
+    try {
+      final resp = await _client.get(
+        "/geo/1.0/direct",
+        queryParameters: {
+          "q": search,
+          "limit": limit,
+          'appid': "ea5e57629b00206a154c5eeb3dade93e",
+        },
+      );
+
+      debugPrint("resp : ${resp.data}");
+
+      return (resp.data as List)
+          .map(((res) => LocationModel.fromJson(res)))
+          .toList();
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
