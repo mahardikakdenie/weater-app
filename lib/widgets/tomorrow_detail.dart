b3843ca063e1330b28801518d7de4f88336db999
mahardikakdenie <kucingemas744@gmail.com>
2025-11-02 19:29:11 +0700

    feat:create tomorrow details widget build

---
+++ lib/widgets/tomorrow_detail.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TomorrowDetail extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final String value;
+  const TomorrowDetail({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, size: 18, color: Colors.white70),
+        const SizedBox(height: 4),
+        Text(
+          value,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 2),
+        Text(
+          label,
+          style: const TextStyle(color: Colors.white70, fontSize: 10),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+}
